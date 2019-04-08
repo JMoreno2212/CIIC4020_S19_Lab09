dbbc84421f03aab68e1d4a2b91a9082e6c1fe43a
Jesus Moreno <jesus.moreno@upr.edu>
2019-04-08 09:19:27 -0400

    Exercises 1 - 3 done!

---
+++ src/testerClasses/IntegerComparator2.java
@@ -0,0 +1,12 @@
+package testerClasses;
+
+import java.util.Comparator;
+
+public class IntegerComparator2 implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o2, Integer o1) {
+		return o1.compareTo(o2);
+	}
+
+}
